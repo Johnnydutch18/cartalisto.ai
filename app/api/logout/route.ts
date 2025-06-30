d40b1ea138f047874c8f80f378c997b5ac98b711
Johnnydutch18 <johnnydutch18@gmail.com>
2025-06-30 09:02:24 +0000

    finally

--- app/api/logout/route.ts
+++ app/api/logout/route.ts
@@ -1,46 +1,64 @@
 [{
 	"resource": "/workspaces/cartalisto.ai/app/api/logout/route.ts",
 	"owner": "typescript",
-	"code": "2769",
+	"code": "2339",
 	"severity": 8,
-	"message": "No overload matches this call.\n  Overload 1 of 2, '(supabaseUrl: string, supabaseKey: string, options: SupabaseClientOptions<\"public\"> & { cookieOptions?: CookieOptionsWithName | undefined; cookies: CookieMethodsServerDeprecated; cookieEncoding?: \"raw\" | ... 1 more ... | undefined; }): SupabaseClient<...>', gave the following error.\n    Property 'get' is missing in type 'Promise<ReadonlyRequestCookies>' but required in type 'CookieMethodsServerDeprecated'.\n  Overload 2 of 2, '(supabaseUrl: string, supabaseKey: string, options: SupabaseClientOptions<\"public\"> & { cookieOptions?: CookieOptionsWithName | undefined; cookies: CookieMethodsServer; cookieEncoding?: \"raw\" | ... 1 more ... | undefined; }): SupabaseClient<...>', gave the following error.\n    Property 'getAll' is missing in type 'Promise<ReadonlyRequestCookies>' but required in type 'CookieMethodsServer'.",
+	"message": "Property 'get' does not exist on type 'Promise<ReadonlyRequestCookies>'.",
 	"source": "ts",
-	"startLineNumber": 10,
-	"startColumn": 7,
-	"endLineNumber": 10,
-	"endColumn": 14,
+	"startLineNumber": 13,
+	"startColumn": 36,
+	"endLineNumber": 13,
+	"endColumn": 39,
 	"relatedInformation": [
 		{
-			"startLineNumber": 31,
-			"startColumn": 5,
-			"endLineNumber": 31,
-			"endColumn": 8,
-			"message": "'get' is declared here.",
-			"resource": "/workspaces/cartalisto.ai/node_modules/.pnpm/@supabase+ssr@0.6.1_@supabase+supabase-js@2.50.2/node_modules/@supabase/ssr/dist/main/types.d.ts"
-		},
+			"startLineNumber": 13,
+			"startColumn": 36,
+			"endLineNumber": 13,
+			"endColumn": 39,
+			"message": "Did you forget to use 'await'?",
+			"resource": "/workspaces/cartalisto.ai/app/api/logout/route.ts"
+		}
+	]
+},{
+	"resource": "/workspaces/cartalisto.ai/app/api/logout/route.ts",
+	"owner": "typescript",
+	"code": "2339",
+	"severity": 8,
+	"message": "Property 'set' does not exist on type 'Promise<ReadonlyRequestCookies>'.",
+	"source": "ts",
+	"startLineNumber": 15,
+	"startColumn": 23,
+	"endLineNumber": 15,
+	"endColumn": 26,
+	"relatedInformation": [
 		{
-			"startLineNumber": 11,
-			"startColumn": 5,
-			"endLineNumber": 11,
-			"endColumn": 12,
-			"message": "The expected type comes from property 'cookies' which is declared here on type 'SupabaseClientOptions<\"public\"> & { cookieOptions?: CookieOptionsWithName | undefined; cookies: CookieMethodsServerDeprecated; cookieEncoding?: \"raw\" | ... 1 more ... | undefined; }'",
-			"resource": "/workspaces/cartalisto.ai/node_modules/.pnpm/@supabase+ssr@0.6.1_@supabase+supabase-js@2.50.2/node_modules/@supabase/ssr/dist/main/createServerClient.d.ts"
-		},
+			"startLineNumber": 15,
+			"startColumn": 23,
+			"endLineNumber": 15,
+			"endColumn": 26,
+			"message": "Did you forget to use 'await'?",
+			"resource": "/workspaces/cartalisto.ai/app/api/logout/route.ts"
+		}
+	]
+},{
+	"resource": "/workspaces/cartalisto.ai/app/api/logout/route.ts",
+	"owner": "typescript",
+	"code": "2339",
+	"severity": 8,
+	"message": "Property 'delete' does not exist on type 'Promise<ReadonlyRequestCookies>'.",
+	"source": "ts",
+	"startLineNumber": 16,
+	"startColumn": 48,
+	"endLineNumber": 16,
+	"endColumn": 54,
+	"relatedInformation": [
 		{
-			"startLineNumber": 36,
-			"startColumn": 5,
-			"endLineNumber": 36,
-			"endColumn": 11,
-			"message": "'getAll' is declared here.",
-			"resource": "/workspaces/cartalisto.ai/node_modules/.pnpm/@supabase+ssr@0.6.1_@supabase+supabase-js@2.50.2/node_modules/@supabase/ssr/dist/main/types.d.ts"
-		},
-		{
-			"startLineNumber": 77,
-			"startColumn": 5,
-			"endLineNumber": 77,
-			"endColumn": 12,
-			"message": "The expected type comes from property 'cookies' which is declared here on type 'SupabaseClientOptions<\"public\"> & { cookieOptions?: CookieOptionsWithName | undefined; cookies: CookieMethodsServer; cookieEncoding?: \"raw\" | ... 1 more ... | undefined; }'",
-			"resource": "/workspaces/cartalisto.ai/node_modules/.pnpm/@supabase+ssr@0.6.1_@supabase+supabase-js@2.50.2/node_modules/@supabase/ssr/dist/main/createServerClient.d.ts"
+			"startLineNumber": 16,
+			"startColumn": 48,
+			"endLineNumber": 16,
+			"endColumn": 54,
+			"message": "Did you forget to use 'await'?",
+			"resource": "/workspaces/cartalisto.ai/app/api/logout/route.ts"
 		}
 	]
 }]
