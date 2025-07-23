7a88490ca662c81b0d182c43a04d307b5c5832d8
Johnnydutch18 <johnnydutch18@gmail.com>
2025-07-23 09:41:19 +0000

    fdj

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,21 @@
-// tailwind.config.js
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
+/** @type {import("tailwindcss").Config} */
 module.exports = {
   content: [
-    './app/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./app/**/*.{ts,tsx,js,jsx}",
+    "./components/**/*.{ts,tsx,js,jsx}",
+    "./node_modules/@shadcn/ui/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Manrope", ...fontFamily.sans],
+      },
+    },
   },
-  plugins: [],
-}
+  plugins: [
+    require("tailwindcss-animate"),
+    require("@tailwindcss/forms"),
+  ],
+};
