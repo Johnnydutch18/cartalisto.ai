30e88c0984e31af60e59ce809289109445d0e921
Johnnydutch18 <johnnydutch18@gmail.com>
2025-07-25 07:27:26 +0000

    sfag

--- app/api/create-billing-portal-session/route.ts
+++ app/api/create-billing-portal-session/route.ts
@@ -5,7 +5,7 @@ import { createServerClient } from '@supabase/ssr';
 import { cookies } from 'next/headers';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2025-04-30.basil',
+  apiVersion: '2025-06-30.basil',
 });
 
 export async function POST() {
